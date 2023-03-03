abd48c9e3e4f788d334e105414cfb8ed30d9a43f
Buzhak <Byzhak@ya.ru>
2023-03-03 22:45:24 +0200

    add user and admin recipes trsts

--- backend/tests/common.py
+++ backend/tests/common.py
@@ -1,22 +1,29 @@
 from rest_framework.test import APIClient
 
 from recipes.models import Tag, Product
+from users.models import User
 
 
 PASSWORD = "random123"
+NAME = "random"
 
-def auth_client(username: str, email: str):
+def auth_client(username: str, email: str, is_admin=False):
     data = {
         "email": email,
         "username": username,
-        "first_name": "random",
-        "last_name": "random",
+        "first_name": NAME,
+        "last_name": NAME,
         "password": PASSWORD
     }
+    # client = APIClient()
+    # client.post('/api/users/', data=data)
+    if is_admin:
+        User.objects.create_superuser(**data)
+    else:
+        User.objects.create_user(**data)
     client = APIClient()
-    client.post('/api/users/', data=data)
     data_login = {
-        "password": "random123",
+        "password": PASSWORD,
         "email": email 
     }
     response = client.post('/api/auth/token/login/', data=data_login)
@@ -26,6 +33,7 @@ def auth_client(username: str, email: str):
     auth_client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
     
     return auth_client
+
 
 def create_tag_data():
     data = [
@@ -52,22 +60,18 @@ def create_tag_data():
 
 def create_product():
     Product.objects.create(
-        name='one',
-        measurement_unit='two'
+        name='Картошка',
+        measurement_unit='шт.'
     )
 
 
 def create_recipe_data(auth_client):
     create_product()
     data = {
-        "ingredients": [
-            {
-            "id": 1,
-            "amount": 10
-            }
-        ],
-        "name": "string",
-        "text": "string",
-        "cooking_time": 1
-    }
-    auth_client.post('/api/recipes/', data = data)
+            'ingredients': [],
+            'name': 'some_name',
+            'tags': [],
+            'cooking_time': 1
+        }
+    response = auth_client.post('/api/recipes/', data, format='json')
+    return response
--- backend/tests/test_recipe.py
+++ backend/tests/test_recipe.py
@@ -2,13 +2,15 @@ import pytest
 from rest_framework.test import APIClient
 from django.shortcuts import get_object_or_404
 
-from .common import auth_client, create_product, create_tag_data
+from .common import auth_client, create_product, create_tag_data, create_recipe_data
 from recipes.models import Product, Tag, Recipe
 
 
 class Test_recipes_api():
     list = '/api/recipes/'
     detail = '/api/recipes/1/'
+    detail_2 = '/api/recipes/2/'
+
     
     @pytest.mark.django_db(transaction=True)
     def test_recipe_api(self, client):
@@ -37,29 +39,32 @@ class Test_recipes_api():
             f'DELETE метод `{self.detail}` должен быть доступен только аутентифицированным пользователям'
         )
 
-        new_user = auth_client('user', 'user@ya.ru')
-        response = new_user.get(self.list)
+        user_1 = auth_client('user', 'user@ya.ru')
+        user_2 = new_user = auth_client('user_2', 'user_2@ya.ru')
+        admin = auth_client('admin', 'admin@ya.ru', is_admin=True)
+        response = user_1.get(self.list)
 
         assert response.status_code == 200, (
             f'GET метод `{self.list}` должен юбыть доступен всем пользователям'
         )
 
-        data = {
-            'ingredients': [],
-            'name': 'some_name',
-            'tags': [],
-            'cooking_time': 1
-        }
-        response = new_user.post(self.list, data, format='json')
+
+        response = create_recipe_data(user_1)
 
         assert response.status_code == 201, (
             f'POST метод `{self.list}` должен быть доступен только аутентифицированным пользователям'
         )
 
-        response = new_user.get(self.detail)
+        response = user_1.get(self.detail)
 
         assert response.status_code == 200, (
             f'GET метод `{self.detail}` должен быть доступен только аутентифицированным пользователям'
+        )
+
+        response = user_2.get(self.detail)
+
+        assert response.status_code == 200, (
+            f'GET метод `{self.detail}` должен быть доступен другим аутентифицированным пользователям'
         )
 
         data_update = {
@@ -68,10 +73,16 @@ class Test_recipes_api():
             'tags': [],
             'cooking_time': 100
         }
-        response = new_user.patch(self.detail, data_update, format='json')
+        response = user_1.patch(self.detail, data_update, format='json')
 
         assert response.status_code == 200, (
-            f'PATCH метод `{self.detail}` должен быть доступен только аутентифицированным пользователям'
+            f'PATCH метод `{self.detail}` должен быть доступен только автору рецепта'
+        )
+
+        response = user_2.patch(self.detail, data_update, format='json')
+
+        assert response.status_code == 403, (
+            f'PATCH метод `{self.detail}` НЕ должен быть доступен другим аутентифицированным пользователям'
         )
         
         recipe = get_object_or_404(Recipe, id=1)
@@ -80,12 +91,32 @@ class Test_recipes_api():
             f'Метод PATCH не работает. Нет изменения записи в БД'
         )
 
-        response = new_user.delete(self.detail)
+        response = user_2.delete(self.detail)
+        
+        assert response.status_code == 403, (
+            f'DELETE метод `{self.detail}` НЕ должен быть доступен другим аутентифицированным пользователям'
+        )
+
+        response = user_1.delete(self.detail)
         
         assert response.status_code == 204, (
-            f'DELETE метод `{self.detail}` должен быть доступен только аутентифицированным пользователям'
+            f'DELETE метод `{self.detail}` должен быть доступен только автору рецепта'
         )
 
         assert not Recipe.objects.filter(id=1).exists(), (
             'запись должна удаляться из БД после успешного DELETE метода'
+        )
+
+        create_recipe_data(user_1)
+
+        response = admin.patch(self.detail_2, data_update, format='json')
+
+        assert response.status_code == 200, (
+            f'PATCH метод `{self.detail_2}` должен быть доступен админу'
+        )
+        
+        response = admin.delete(self.detail_2)
+        
+        assert response.status_code == 204, (
+            f'DELETE метод `{self.detail_2}` должен быть доступен админу'
         )
