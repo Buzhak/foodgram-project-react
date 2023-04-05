9e47542002a8aed7d8fff1e2a87f282a11f41207
Buzhak <Byzhak@ya.ru>
2023-04-05 15:21:53 +0300

    fix

--- backend/recipes/management/commands/load_csv.py
+++ backend/recipes/management/commands/load_csv.py
@@ -1,10 +1,11 @@
+import pathlib
 from csv import DictReader
 
 from django.conf import settings
 from django.core.management import BaseCommand
 from recipes.models import Product
 
-DATA_PATH = '/static/data/'
+DATA_PATH = pathlib.Path('/static/data/ingredients.csv')
 SCV = {
     Product: 'ingredients.csv',
 }
@@ -14,7 +15,7 @@ class Command(BaseCommand):
     '''Загрузка тестовых данных из csv в БД.'''
     def handle(self, *args, **kwargs):
         for model, file in SCV.items():
-            with open(f'{settings.BASE_DIR}{DATA_PATH}{file}',
+            with open(f'{settings.BASE_DIR}{DATA_PATH}',
                       'r', encoding='utf-8') as csv_file:
                 reader = DictReader(csv_file)
                 model.objects.bulk_create(model(**data) for data in reader)
